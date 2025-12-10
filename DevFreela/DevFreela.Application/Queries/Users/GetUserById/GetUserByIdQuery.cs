4abb1262e21d108f0eb4d64ba21b119e9de32fd0
Lucas siqueira <lucassiqueira0763@hotmail.com>
2025-12-10 17:46:18 +0000

    refactor(users): migrate UsersController to CQRS pattern
    Removes the dependency on IUserService and implements MediatR commands and queries for user operations.
    
    Key changes:
    - Refactored GetById, Post (Create User), and PostSkill endpoints to use IMediator.
    - Implemented GetUserByIdQuery and InsertUserSkillCommand.
    - Decoupled the controller from the service layer, aligning with the clean architecture/CQRS standard of the project.

---
+++ DevFreela/DevFreela.Application/Queries/Users/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.Users.GetUserById
+{
+    public class GetUserByIdQuery : IRequest<ResultViewModel<UserViewModel>>
+    {
+        public GetUserByIdQuery(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; private set; }
+        
+    }
+}
